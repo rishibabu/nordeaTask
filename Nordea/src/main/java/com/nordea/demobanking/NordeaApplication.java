8b3ad80177405aede2f12df2b670fcd7673fd8a1
Rishibabu <rishibabu.s027@gmail.com>
2022-05-07 09:39:56 +0530

    first commit

---
+++ Nordea/src/main/java/com/nordea/demobanking/NordeaApplication.java
@@ -0,0 +1,13 @@
+package com.nordea.demobanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NordeaApplication.class, args);
+	}
+
+}
---
+++ Nordea/src/test/java/com/nordea/demobanking/NordeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordea.demobanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
