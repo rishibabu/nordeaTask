3082b3597c346a0fef3c420243484128e265cd7e
Rishibabu <rishibabu.s027@gmail.com>
2022-05-11 16:34:09 +0530

    psuhing latest changes

--- Nordea/src/main/java/com/nordea/demobanking/NordeaConfig.java
+++ Nordea/src/main/java/com/nordea/demobanking/NordeaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class NordeaConfig {
+public class NordeaConfig  {
 	
 	@Bean
 	public WebClient webClient()
